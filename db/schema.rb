18aeaf25723ea83f4aade956ad263a54344b6932
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-02-09 16:23:15 +0900

    create listings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208110605) do
+ActiveRecord::Schema.define(version: 20170209070932) do
+
+  create_table "listings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "home_type"
+    t.string   "pet_type"
+    t.string   "pet_size"
+    t.integer  "breeding_years"
+    t.string   "address"
+    t.string   "listing_title"
+    t.text     "listing_content", limit: 65535
+    t.integer  "price_pernight"
+    t.boolean  "active"
+    t.integer  "user_id"
+    t.datetime "created_at",                    null: false
+    t.datetime "updated_at",                    null: false
+    t.index ["user_id"], name: "index_listings_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "email",                  default: "", null: false
@@ -35,4 +51,5 @@ ActiveRecord::Schema.define(version: 20170208110605) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "listings", "users"
 end
