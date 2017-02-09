7884b939ef4deb011459d5e93e4757fb2bc2e1e2
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-02-09 19:13:02 +0900

    add basics action to of listings to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: :show
 
   resources :listings, only: [:new, :create, :edit]
+
+  get 'manage-listing/:id/basics' => 'listings#basics', as: 'manage-listing_basics'
 end
