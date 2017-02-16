93139add21ee2bb0f23f72b1ecd4c4a98471627a
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-02-16 11:17:52 +0900

    add column to listings table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170212022358) do
+ActiveRecord::Schema.define(version: 20170216020601) do
 
   create_table "listings", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "home_type"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170212022358) do
     t.integer  "user_id"
     t.datetime "created_at",                    null: false
     t.datetime "updated_at",                    null: false
+    t.float    "latitude",        limit: 24
+    t.float    "longitude",       limit: 24
     t.index ["user_id"], name: "index_listings_on_user_id", using: :btree
   end
 
