4efea33cc275d88e6347c5a3b759dfa84310d341
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-02-27 19:15:32 +0900

    add association of reviews to listing.rb

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_many :photos
   has_many :reservations
+  has_many :reviews
 
   validates :home_type, presence: true
   validates :pet_type, presence: true
