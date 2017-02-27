491f2ce270fb8f84d8b0bdba0486d5a075ad85e7
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-02-27 16:58:45 +0900

    use %i to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :phone_number, :description, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i(name email phone_number description image))
   end
 end
