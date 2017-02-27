1cc08cdb4a20b4d8d2f05efc44ab3b62d4d648bd
前山　大次郎 <daijiro@maeyamadaijirou-no-MacBook-Air.local>
2017-02-27 17:47:32 +0900

    add access_deny to listing_controller.rb

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_listing, only: %i(show update basics description address price photos calendar bankaccount publish)
+  before_action :access_deny, only: %i(basics description address price photos calendar bankaccount publish)
 
   def index
     @listings = current_user.listings
@@ -65,6 +66,12 @@ class ListingsController < ApplicationController
 
   def set_listing
     @listing = Listing.find(params[:id])
+  end
+
+  def access_deny
+    if !(current_user == @listing.user)
+      redirect_to root_path, notice: "他人の編集ページにはアクセスできません"
+    end
   end
 
 end
